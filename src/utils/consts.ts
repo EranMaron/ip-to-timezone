09b6ba355094226b6b72bbfff89d67a38e335425
EranMaron <eran9maron@gmail.com>
2025-05-29 13:31:42 +0300

    chore: add local storage key name

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -2,3 +2,4 @@ export const IP_FIELDS = 'status,message,country,countryCode,timezone,query';
 export const SINGLE_TIME_ZONE_API_URL = 'http://ip-api.com/json/';
 export const BATCH_TIME_ZONE_API_URL = 'http://ip-api.com/batch';
 export const FLAG_CDN_URL = 'https://flagcdn.com/28x21/';
+export const STORAGE_KEY = 'ip-lookup-data';
